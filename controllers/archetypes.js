ae4b5a5c0a5a807e0b999f9775a3a6f90a73a2ce
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-08-11 00:05:15 -0400

    update: edit response for archetypes endpoint

--- controllers/archetypes.js
+++ controllers/archetypes.js
@@ -1,12 +1,17 @@
 const handlearchetypes = (req, res, db) => {
-  return db
-    .select("name")
-    .from("urlcheck")
-    .orderby("name", "desc")
-    .then((data) => {
-      // Convert db results from json to js
-      let results = JSON.parse(JSON.stringify(data));
-      res.status(200).json(results);
-    });
+  const { letter } = req.body;
+  try {
+    return db
+      .select("archetypename", "link", "url")
+      .from("urlcheck")
+      .where("letter", letter)
+      .then((data) => {
+        // Convert db results from json to js
+        let results = JSON.parse(JSON.stringify(data));
+        res.status(200).json(results);
+      });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 export default handlearchetypes;
