098e9943f5fdd23f9a03c6d09bbf49b24c7e0f30
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-08-15 00:50:33 -0400

    bugfix: fix cors issue

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import express from "express";
 import cors from "cors";
 import knex from "knex";
 import dotenv from "dotenv";
+var allowedOrigins = ["http://localhost:3000", "https://sycendin.github.io/"];
 const sslRedirect = herokuSSLRedirect.default;
 dotenv.config();
 const db = knex({
@@ -22,7 +23,22 @@ const db = knex({
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(sslRedirect());
 app.get("/", (req, res) => {
   res.send("Server is working!");
